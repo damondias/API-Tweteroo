eba38843c9575f2b97f18d8a8c7de30aa43be060
Damon <damondias@gmail.com>
2022-01-24 00:01:14 -0400

    Armazenando dados em memoria e criando o POST da rota sign-up

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ const app = express();
 app.use(json());
 app.use(cors());
 
+const users = [];
+let tweets = [];
+
+app.post("/sign-up", (req, res) => {
+    const user = req.body;
+    users.push(user);
+    res.send("OK");
+  });
+  
 app.listen(5000, () => {
     console.log("Rodando API tweteroo em http://localhost:5000");
 })
