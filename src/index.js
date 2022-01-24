cbe5482e33a47858804d999aaf29579ee0544a00
Damon <damondias@gmail.com>
2022-01-24 00:21:08 -0400

    Criando POST da rota tweets

--- src/index.js
+++ src/index.js
@@ -12,8 +12,19 @@ app.post("/sign-up", (req, res) => {
     const user = req.body;
     users.push(user);
     res.send("OK");
-  });
+});
+
+app.post("/tweets", (req, res) => {
+    const tweetUser = {
+      username: req.body.username,
+      tweet: req.body.tweet,
+      avatar: users.find((user) => user.username === req.body.username).avatar,
+    };
   
+    tweets.push(tweetUser);
+    res.send("OK");
+});
+
 app.listen(5000, () => {
     console.log("Rodando API tweteroo em http://localhost:5000");
 })
