17f1a37b6067970b79974bb685f6b5b2e03ac181
Damon <damondias@gmail.com>
2022-01-23 23:05:59 -0400

    Corrigindo porta e aplicando json e cors no projeto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import express  from "express";
+import express, {json}  from "express";
 import cors from "cors";
 
 const app = express();
+app.use(json());
+app.use(cors());
 
 app.listen(5000, () => {
     console.log("Rodando API tweteroo em http://localhost:5000");
